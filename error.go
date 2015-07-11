a1efed05e6ea890697879800341c622e551713b0
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-07-11 22:29:58 +0200

    Don't export the different errors
    The Is*Error() functions provide enough information.

--- error.go
+++ error.go
@@ -6,51 +6,51 @@ package ini
 
 import "fmt"
 
-type SynthaxError struct {
+type synthaxError struct {
 	LineNumber int
 	Message    string
 }
 
-func (err SynthaxError) Error() string {
+func (err synthaxError) Error() string {
 	return fmt.Sprintf("ini: synthax error on line %d: %s",
 		err.LineNumber, err.Message)
 }
 
-type OverflowError struct {
+type overflowError struct {
 	Value string
 	Type  string
 }
 
-func (err OverflowError) Error() string {
+func (err overflowError) Error() string {
 	return fmt.Sprintf("ini: can't convert '%s' to type %s, it overflows the type",
 		err.Value, err.Type)
 }
 
-type CovertionError struct {
+type covertionError struct {
 	Value string
 	Type  string
 }
 
-func (err CovertionError) Error() string {
+func (err covertionError) Error() string {
 	return fmt.Sprintf("ini: can't convert '%s' to type %s", err.Value, err.Type)
 }
 
 func createSynthaxError(lineNumber int, msg string) error {
-	return SynthaxError{
+	return synthaxError{
 		LineNumber: lineNumber,
 		Message:    msg,
 	}
 }
 
 func createOverflowError(value, t string) error {
-	return OverflowError{
+	return overflowError{
 		Value: value,
 		Type:  t,
 	}
 }
 
 func createCovertionError(value, t string) error {
-	return CovertionError{
+	return covertionError{
 		Value: value,
 		Type:  t,
 	}
@@ -58,18 +58,18 @@ func createCovertionError(value, t string) error {
 
 // IsSynthaxError checks if an error is a synthax error.
 func IsSynthaxError(err error) bool {
-	_, ok := err.(SynthaxError)
+	_, ok := err.(synthaxError)
 	return ok
 }
 
 // IsOverflowError checks if an error is an overflow error.
 func IsOverflowError(err error) bool {
-	_, ok := err.(OverflowError)
+	_, ok := err.(overflowError)
 	return ok
 }
 
 // IsCovertionError checks if an error is a covertion error.
 func IsCovertionError(err error) bool {
-	_, ok := err.(CovertionError)
+	_, ok := err.(covertionError)
 	return ok
 }
